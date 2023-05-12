954644e23a6d4288dfac40dea9935b70818428c7
Aaron Thomas <aarthomas01@gmail.com>
2023-05-12 02:41:03 -0400

    reset database

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
+// const { PrismaClient } = require('@prisma/client');
+// const prisma = new PrismaClient();
 
-async function main() {
-	const user = await prisma.user.findMany();
+// async function main() {
+// 	const user = await prisma.user.findMany();
 
-	console.log(user);
-}
+// 	prisma.user.deleteMany();
+// }
 
-// automatically disconnects prisma client when done
-main()
-	.catch((e) => {
-		console.error(e.message);
-	})
-	.finally(async () => {
-		await prisma.$disconnect();
-	});
+// // automatically disconnects prisma client when done
+// main()
+// 	.catch((e) => {
+// 		console.error(e.message);
+// 	})
+// 	.finally(async () => {
+// 		await prisma.$disconnect();
+// 	});
