7d0dc9f5a801be42ecd4016b6def84b7bd5a8c22
Aaron Thomas <aarthomas01@gmail.com>
2023-07-24 10:05:34 -0400

    fix UserProfile typo

--- components/UserProfile.tsx
+++ components/UserProfile.tsx
@@ -1,20 +1,17 @@
 import { useSession } from 'next-auth/react';
 import { FunctionComponent } from 'react';
-import { FunctionComponent } from 'react';
 
-export const UserProfile: FunctionComponent = () => {
 export const UserProfile: FunctionComponent = () => {
 	const { data: session } = useSession();
 	const user = session?.user;
-	const user = session?.user;
 
 	return (
-		<div className="flex flex-col justify-center mt-10">
+		<div className="mt-10 flex flex-col justify-center">
 			{user?.image && (
 				<img
 					src={user?.image}
 					alt="user profile"
-					className="w-16 h-16 mx-auto rounded-full"
+					className="mx-auto h-16 w-16 rounded-full"
 				/>
 			)}
 			<p className="py-5">{user?.name}</p>
